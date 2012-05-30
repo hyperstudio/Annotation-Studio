b786d069789041089afdf11fd30140da9ed802d4
Jamie Folsom <jamiefolsom@gmail.com>
2012-05-30 10:00:09 -0400

    Renaming app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Monticello::Application.configure do
+Sandbox::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests

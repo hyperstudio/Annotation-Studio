410cf5dfaef377438ff71df9739c5b25bec81cb1
Jamie Folsom <jamiefolsom@gmail.com>
2012-06-18 11:05:21 -0400

    Adding in initial authentication functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def signed_in?
+    @jwt = JWT.encode(
+        {
+            'consumerKey' => "CONSUMER_KEY",
+            'userId' => "jfolsom",
+            'issuedAt' => "2012-06-14",
+            'ttl' => 86400
+        }, 
+        "secretgoeshere"
+      )
+  end
+  helper_method :signed_in?
+
+  def authenticate
+    redirect_to root_path, notice:"You need to be signed in" unless signed_in?
+  end
+
 end

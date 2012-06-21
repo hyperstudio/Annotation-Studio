e8ce73be08622143743f66653465c19c16c4b9c8
Jamie Folsom <jamiefolsom@gmail.com>
2012-06-21 15:05:05 -0400

    More replacements for app rename.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Sandbox::Application.configure do
+AnnotationStudio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Sandbox::Application.configure do
+AnnotationStudio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests

18a276119274ae90368ab49ead1d20c20c41987c
Jamie Folsom <jamiefolsom@gmail.com>
2012-07-23 15:23:36 -0400

    General app cleanup.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,11 +16,6 @@
 //= require_tree .
 //= require xpath.min
 //= require json2.min
-// require annotator
 //= require annotator-full.min
-// require annotator.touch.min
-// require heatmap
-// require cssify
-// require getPath
-// require selectedText
+//= require annotator.touch.min
 //= require boot-docs

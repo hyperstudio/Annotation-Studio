efcbd2d607b17895c5b66d7212d9910727be0296
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-22 11:15:40 -0400

    Complete rewrite of the sidebar, cleanup of document view.

---
+++ app/assets/javascripts/documents.js
@@ -0,0 +1,8 @@
+//= require twitter/bootstrap
+//= require xpath.min
+//= require json2.min
+//= require showdown
+//= require annotator-full.min
+//= require markdown
+//= require annotator.touch.min
+//= require boot-docs

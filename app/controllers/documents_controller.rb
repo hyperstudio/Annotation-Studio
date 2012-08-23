887218b8d9bdea2f3abe951e4a1256599b2dd42c
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-23 11:47:48 -0400

    Un-nest documents from collections.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,12 +1,12 @@
 class DocumentsController < ApplicationController
 
   before_filter :authenticate
-  before_filter :get_collection
+  # before_filter :get_collection
 
-  # GET /collection/1/documents
-  # GET /collection/1/documents.json
+  # GET /documents
+  # GET /documents.json
   def index
-    @documents = @collection.documents
+    @documents = Document.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -25,8 +25,8 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # GET /collection/1/documents/new
-  # GET /collection/1/documents/new.json
+  # GET /documents/new
+  # GET /documents/new.json
   def new
     @document = Document.new
 
@@ -36,20 +36,20 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # GET /collection/1/documents/1/edit
+  # GET /documents/1/edit
   def edit
     @document = Document.find(params[:id])
   end
 
-  # POST /collection/1/documents
-  # POST /collection/1/documents.json
+  # POST /documents
+  # POST /documents.json
   def create
     @document = @collection.documents.build(params[:document])
 
     respond_to do |format|
       if @document.save
-        format.html { redirect_to @collection, notice: 'Document was successfully created.' }
-        format.json { render json: @collection, status: :created, location: @document }
+        format.html { redirect_to @document, notice: 'Document was successfully created.' }
+        format.json { render json: @document, status: :created, location: @document }
       else
         format.html { render action: "new" }
         format.json { render json: @document.errors, status: :unprocessable_entity }
@@ -57,14 +57,14 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # PUT /collection/1/documents/1
-  # PUT /collection/1/documents/1.json
+  # PUT /documents/1
+  # PUT /documents/1.json
   def update
     @document = Document.find(params[:id])
 
     respond_to do |format|
       if @document.update_attributes(params[:document])
-        format.html { redirect_to @collection, notice: 'Document was successfully updated.' }
+        format.html { redirect_to @document, notice: 'Document was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -73,14 +73,14 @@ class DocumentsController < ApplicationController
     end
   end
 
-  # DELETE /collection/1/documents/1
-  # DELETE /collection/1/documents/1.json
+  # DELETE /documents/1
+  # DELETE /documents/1.json
   def destroy
-    @document = @collection.documents.find(params[:id])
+    @document = Document.find(params[:id])
     @document.destroy
 
     respond_to do |format|
-      format.html { redirect_to @collection }
+      format.html { redirect_to documents_url }
       format.json { head :no_content }
     end
   end

08b781b87d1d53a0cd790f7e41d8b570b8eda8e4
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-24 16:58:20 -0400

    Update user model to allow email confirmation. Update mail config to get devise, send grid working. Not sure about latter yet.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,7 @@
 class Document < ActiveRecord::Base
   belongs_to :collection
   extend FriendlyId
-  friendly_id :title, use: :slugged  
+  friendly_id :title, use: [:slugged, :history]
+  acts_as_taggable_on :rep_privacy, :rep_group
+  
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,16 @@ AnnotationStudio::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'annotationstudio.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "587",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN'],
+    :enable_starttls_auto => true
+  }  
 end

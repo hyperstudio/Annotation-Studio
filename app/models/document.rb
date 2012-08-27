78abd3fe3b650beb345ea61bf456e23b4ef4c2b2
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-27 16:53:50 -0400

    Permissions, permission strings, move markdown cheatsheet link to navbar.

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,7 +3,7 @@ class Document < ActiveRecord::Base
   attr_accessible :title, :text, :user_id, :rep_privacy_list, :rep_group_list, :new_group, :author, :edition, :publisher, :source, :rights_status
   extend FriendlyId
   friendly_id :title, use: [:slugged, :history]
-  acts_as_taggable_on :rep_privacy, :rep_group, :document_tag
+  acts_as_taggable_on :rep_privacy, :rep_group, :courses, :semesters, :genres, :categories
 
   def new_group=(group_name)
     rep_group_list << group_name unless group_name.nil? || group_name.empty?

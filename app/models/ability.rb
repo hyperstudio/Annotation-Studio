5cb9f267a7109d649fa92ddc36451bf80e571993
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-27 14:52:40 -0400

    Now documents are owned by users, collections are gone, some abilities are copied from minimize; renamed document description > document text.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,28 +3,33 @@ class Ability
   include Repertoire::Groups::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
-	defaults_for user
+    user ||= User.new # guest user (not logged in)
+
+    # If CanCan doesn't find a match for the above, it falls through 
+    # to the default abilities provided by Repertoire Groups:
+    defaults_for user
+
+    if user.has_role? :admin
+      can :manage, :all
+    elsif user.has_role? :teacher
+      can :create, Document
+      can :update, Document do |tors|
+        tors.user.id == user.id
+      end
+    elsif user.has_role? :student
+      cannot :read, [Document] do |tors|
+        tors.rep_privacy_list.detect {|t| t == 'review'}
+      end
+      cannot :create, Document
+      can :update, Document do |tors|
+        !(user.rep_group_list & tors.rep_group_list).empty?
+      end
+    else
+      cannot :manage, :all
+      can :read, [Document] do |tors|
+        tors.rep_privacy_list.detect {|t| t == 'public'}
+      end
+    end
+    can :index, Document
   end
 end

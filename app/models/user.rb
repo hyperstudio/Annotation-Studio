66aa3589748c05b443d1e2422278d1de4448525c
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-30 11:13:52 -0400

    Permissions improvements. Annotation highlight comment icon on sidebar item click.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :lastname
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :lastname, :rep_privacy_list
 
   acts_as_role_user
-  acts_as_taggable_on :rep_group
+  acts_as_taggable_on :rep_group, :rep_privacy
   
   def fullname
     "#{firstname} #{lastname}"

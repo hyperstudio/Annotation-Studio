cec1e8a71c8b86359756c7c738270951ae8de3fb
Jamie Folsom <jamiefolsom@gmail.com>
2012-08-31 12:28:55 -0400

    Tweak production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ AnnotationStudio::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,10 +46,10 @@ AnnotationStudio::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( sidebar.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!

6d96f67055dc057e68977ce69046b3a4f1b1c25d
Jamie Folsom <jamiefolsom@gmail.com>
2012-09-02 22:52:36 -0400

    Permissions, tags, tweaks to sidebar.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,22 +11,24 @@ class Ability
 
     if user.has_role? :admin
       can :manage, :all
+
     elsif user.has_role? :teacher
       can :create, Document
       can :update, Document do |tors|
         tors.user.id == user.id
       end
+
     elsif user.has_role? :student
-      # cannot :read, [Document] do |tors|
-      #   tors.rep_privacy_list.detect {|t| t == 'review'}
-      # end
+      cannot :read, [Document] do |tors|
+        tors.rep_privacy_list.detect {|t| t == 'review'}
+      end
       can :read, Document do |tors|
         !(user.rep_group_list & tors.rep_group_list).empty?
       end
 
       # Why not?
       cannot :create, Document
-      # can :create, Document
+      # cannot :manage, Document
 
       # This on the other hand, should not happen...
       # can :update, Document do |tors|

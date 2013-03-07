4c31cf0dc39684b12b021968f18207d475dd7fb7
Jamie Folsom <jamiefolsom@gmail.com>
2013-03-07 12:00:19 -0500

    Add batch actions to index, and group checkboxes to edit forms in AA user mgmt ui.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -9,8 +9,21 @@ ActiveAdmin.register User, :as => "Student" do
   filter :updated_at
   filter :taggings_tag_name, :as => :check_boxes, :collection => proc { User.rep_group_counts.map{|t| t.name} }
 
-  index do
-    column :id
+  batch_action :approve do |selection|
+    User.find(selection).each do |user|
+      user.set_roles = ['student']
+    end
+  end
+
+  batch_action :make_admin do |selection|
+    User.find(selection).each do |user|
+      user.set_roles = ['admin']
+    end
+  end
+    
+  index do |t|
+    selectable_column
+    id_column
     column :fullname
     column :affiliation
     column "Groups", :rep_group_list
@@ -32,10 +45,17 @@ ActiveAdmin.register User, :as => "Student" do
       f.input :lastname, :as => :string
       f.input :affiliation, :as => :string
       f.input :email, :as => :string
-      f.input :rep_group_list, :label => "Groups",
-                         :as => :select,
-                         :multiple => :true,
-                         :collection => ActsAsTaggableOn::Tag.all.map(&:name)
+      f.input :rep_group_list, 
+      :label => "Add/remove existing groups",
+        # :input_html => { :size => 50 },
+        :as => :check_boxes,
+        # :multiple => :true,
+        :collection => ActsAsTaggableOn::Tag.all.map(&:name)
+      f.input :rep_group_list, 
+        :value => nil,
+        :placeholder_text => "Type new group here",
+        :as => :string,
+        :label => "Create (and add user to) a new group"
     end
     f.buttons
   end

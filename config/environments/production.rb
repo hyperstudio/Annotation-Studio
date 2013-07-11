636aef2d6c2bc34d475682de9fa5e8106c05cd83
Seth Woodworth <seth@sethish.com>
2013-07-11 13:40:56 -0400

    moving domain name of the current server to an env variable by way of application.yml and updating mailers to match

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ AnnotationStudio::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'annotationstudio.mit.edu' }
+  config.action_mailer.default_url_options = { :host => ENV['SITE_DOMAIN'] }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"  

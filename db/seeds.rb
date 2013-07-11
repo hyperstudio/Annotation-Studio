hyperstudio/stanford

adding three initial group Roles: Admin Editor and Student

db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create initial groups for the Annotation Studio application
+Role.where(name: 'Admin').first_or_create
+Role.where(name: 'Editor').first_or_create
+Role.where(name: 'Student').first_or_create

hyperstudio/Annotation-Studio

db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create initial groups for the Annotation Studio application
+Role.where(name: 'admin').first_or_create
+Role.where(name: 'editor').first_or_create
+Role.where(name: 'student').first_or_create
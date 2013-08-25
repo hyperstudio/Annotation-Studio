c0b093068f8770caf44bdfcbb261c0483849fcfc
Jamie Folsom <jamiefolsom@gmail.com>
2013-08-25 13:56:33 -0400

    Make user dashboard the default view on login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,15 @@ class UsersController < ApplicationController
   before_filter :authenticate
 
   def show
-    @user = User.find(params[:id])
-    
-    # respond_to do |format|
-    #   format.html # show.html.erb
-    #   format.json { render json: @user }
-    # end
+    if params[:id].nil? # if there is no user id in params, show current one
+        @user = current_user
+    else
+        @user = User.find params[:id]
+    end
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
     
     gon.rabl "app/views/users/show.rabl", as: "user"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 AnnotationStudio::Application.routes.draw do
-  root :to => 'documents#index'
-
-  ActiveAdmin.routes(self)
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-
   devise_for :users
 
-  # Allow annotators to view their work
-  match 'users/:id' => 'users#show'
-
   resources :documents
+  resources :users, only: [:show, :edit]
+
+  authenticated :user do
+    root :to => "users#show"
+  end
+
+  unauthenticated :user do
+    devise_scope :user do 
+      get "/" => "devise/sessions#new"
+    end
+  end
+
+  ActiveAdmin.routes(self)
+  devise_for :admin_users, ActiveAdmin::Devise.config
 end

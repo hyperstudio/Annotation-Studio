e916aadadcbf650fb33d3f6a90a6ee83e8606a62
Jamie Folsom <jamiefolsom@gmail.com>
2013-09-04 14:46:48 -0400

    Add annotations controller, routes, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ AnnotationStudio::Application.routes.draw do
   authenticated :user do
     root :to => "users#show"
     get 'dashboard', to: 'users#show', as: :dashboard
+    get 'annotations', to: 'annotations#index'
+    get 'annotations/:id', to: 'annotations#show'
   end
 
   unauthenticated :user do
-    root :to => "users#show"
     devise_scope :user do 
       get "/" => "devise/sessions#new"
     end
---
+++ test/unit/helpers/annotations_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class AnnotationsHelperTest < ActionView::TestCase
+end

6813e3ce69751c0420e7809fff3a8274b4333f10
Jamie Folsom <jamiefolsom@gmail.com>
2013-09-09 15:13:52 -0400

    Delayed jobs.

--- db/schema.rb
+++ db/schema.rb
@@ -53,22 +53,6 @@ ActiveRecord::Schema.define(:version => 20130903205508) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "documents", :force => true do |t|
     t.string   "title"
     t.text     "text"

0b45a7cb71a54f9996e784c44da0718ce435e801
Jamie Folsom <jamiefolsom@gmail.com>
2013-09-10 10:41:28 -0400

    Reintegrate redactor-rails, after gem upgrade, to test on staging.

--- app/assets/javascripts/redactor-config.js
+++ app/assets/javascripts/redactor-config.js
@@ -6,16 +6,16 @@ $(document).ready(
 		if (csrf_param !== undefined && csrf_token !== undefined) {
 			params = csrf_param + "=" + encodeURIComponent(csrf_token);
 		}
-		// $('.redactor').redactor({ 
-		// 	"path":"/assets/redactor-rails",
-		// 	"css":"style.css",
-		// 	"autoresize": "false",
-		// 	"fixed": "true",
-		// 	"fixedTop": "43",
-		// 	"convertDivs": "false",
-		// });
-		// $('.chapter-editor').redactor({
-		// 	allowedTags: ["a", "li"],
-		// });
+		$('.redactor').redactor({ 
+			"path":"/assets/redactor-rails",
+			"css":"style.css",
+			"autoresize": "false",
+			"fixed": "true",
+			"fixedTop": "43",
+			"convertDivs": "false",
+		});
+		$('.chapter-editor').redactor({
+			allowedTags: ["a", "li"],
+		});
 	}
 );

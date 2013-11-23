9b17e125868c6a3085cc41d5ca5870e53809c09b
b3h47pte <mike.bao@berkeley.edu>
2013-11-23 10:09:42 -0800

    Fixed

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,9 @@
-class Image < ActiveRecord::Base
+require 's3file'
+class Image < S3File
+  @BASE_BUCKET = AnnotationStudio::Application::IMAGE_BUCKET
+
+  def self.BASE_BUCKET
+    AnnotationStudio::Application::IMAGE_BUCKET 
+  end
   
 end

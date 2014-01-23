6efbf7edc02bb7f9f694b7248fb74d0313a83153
Jamie Folsom <jamiefolsom@gmail.com>
2014-01-23 12:25:18 -0500

    Make group fields into tag fields.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require twitter/bootstrap
+//= require bootstrap-tagsinput
 //= require jasny-fileinput
 //= require redactor-rails/redactor.min
 //= require redactor-config

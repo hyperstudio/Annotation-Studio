7e2227031ad082aa0ed071f4825d7a3eaa4e2534
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2014-04-06 12:20:11 -0400

    Functional mobile annotations

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,8 +1,5 @@
 //= require xpath.min
 //= require json2.min
 //= require showdown
-//= require annotator-full.min
 //= require markdown
-//= require annotator.touch.min
 //= require boot-docs
-
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,5 +29,22 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+private
+
+  def mobile_device?
+      if session[:mobile_param]
+        session[:mobile_param] == "1"
+      else
+        request.user_agent && request.user_agent.downcase =~ /mobile|iphone|webos|android|blackberry|midp|cldc/ && request.user_agent.downcase !~ /ipad/
+      end
+    end
   
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
+ 
 end

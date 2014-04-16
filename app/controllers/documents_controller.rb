7853b07394def785425e88fc2d1df4bb6d22a03f
Andrew Magliozzi <andrew.magliozzi@gmail.com>
2014-04-16 16:54:06 -0400

    removing rich text editor from mobile touch plugin

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -109,4 +109,20 @@ class DocumentsController < ApplicationController
       i unless cannot? :read, i
     end
   end
+
+    before_filter :prepare_for_mobile
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.env["HTTP_USER_AGENT"] =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+   # request.format = :mobile if mobile_device?
+  end
 end

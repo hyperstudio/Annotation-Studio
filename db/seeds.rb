3129b775d72f2d2edf0d53e3ea01651d3ad15f73
Matthew Sumner <matt.m.sumner@gmail.com>
2014-06-23 10:49:01 -0400

    Better seeding, base database.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,23 @@
 Role.where(name: 'admin').first_or_create
 Role.where(name: 'teacher').first_or_create
 Role.where(name: 'student').first_or_create
+
+email = 'student@example.com'
+password = 'password'
+
+user = User.where(email: email).first_or_initialize
+user.password = user.password_confirmation = password
+user.set_roles = ['student']
+user.save
+
+puts "Created user: #{email}, password: #{password}"
+
+document = Document.where(
+  title: 'example',
+  text: File.read('spec/support/example_files/example.html'),
+  user_id: user.id,
+  state: 'published',
+).first_or_create
+
+document.rep_group_list = 'student'
+document.save

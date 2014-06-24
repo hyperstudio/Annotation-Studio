81808b66b008060a2e73263a34208dfbd0c41ec6
Matthew Sumner <matt.m.sumner@gmail.com>
2014-06-24 08:59:39 -0400

    Show/hide annotations in sidebar.
    Includes:
    * Showing and hiding using jquery after backbone render.
    * This is untested. Feature specs using capybara are not playing well.
    * Sorts the annotations by position in the document.

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -2,213 +2,256 @@ Sidebar = window.Sidebar || {};
 
 // Models
 Sidebar.AnnotationRows = Backbone.Model.extend({
-	defaults: {
-		rows: null,
-	}
+  defaults: {
+    rows: null,
+  }
 });
 
 // Models
 Sidebar.Annotation = Backbone.Model.extend({
-	initialize: function (annotationObject) {
-		this.set(annotationObject);
-	},
-	defaults: {
-		user: null,
-		quote: null,
-		text: null,
-		id: null,
-	}
+  initialize: function(annotationObject) {
+    this.set(annotationObject);
+  },
+  defaults: {
+    user: null,
+    quote: null,
+    text: null,
+    id: null,
+  }
 });
 
 // Collection
 Sidebar.RemoteAnnotationList = Backbone.Collection.extend({
-	model: Sidebar.Annotation,
+  model: Sidebar.Annotation,
     url: 'http://annotations.mit.edu/api/search',
     // url: 'http://localhost:5000/api/search',
-	// comparator: function(annotation) {
-	// 	try {
-	// 		var startOffset = annotation.get("ranges")[0].startOffset;
-	// 	}
-	// 	catch(e) {
-	// 		console.info("startOffset issue." + e.toString());
-	// 	}
-	// 	finally {
-	// 		return startOffset; // change to startOffset
-	// 	}
-	// },
-	initialize: function (options) {
-		//console.info(options);
-		this.fetch({
-			data: options,
-			success: this.fetchSuccess,
-			error: this.fetchError
-		});
-		this.deferred = new $.Deferred();
-		// this.sort();
-	},
+  // comparator: function(annotation) {
+  //   try {
+  //     var startOffset = annotation.get("ranges")[0].startOffset;
+  //   }
+  //   catch(e) {
+  //     console.info("startOffset issue." + e.toString());
+  //   }
+  //   finally {
+  //     return startOffset; // change to startOffset
+  //   }
+  // },
+  initialize: function(options) {
+    //console.info(options);
+    this.fetch({
+      data: options,
+      success: this.fetchSuccess,
+      error: this.fetchError
+    });
+    this.deferred = new $.Deferred();
+    // this.sort();
+  },
     deferred: Function.constructor.prototype,
-    fetchSuccess: function (collection, response) {
+    fetchSuccess: function(collection, response) {
         collection.deferred.resolve();
     },
-    fetchError: function (collection, response) {
+    fetchError: function(collection, response) {
         throw new Error("Fetch did not get annotations from the API");
     }
 });
 
 Sidebar.LocalAnnotationList = Backbone.Collection.extend({
-	model: Sidebar.Annotation,
-	// comparator: function(annotation) {
-	// 	return annotation.get("ranges")[0].startOffset; // change to startOffset
-	// },
-	initialize: function (annotations) {
-		// this.sort();
-	},
+  model: Sidebar.Annotation,
+  // comparator: function(annotation) {
+  //   return annotation.get("ranges")[0].startOffset; // change to startOffset
+  // },
+  initialize: function(annotations) {
+    // this.sort();
+  },
 });
 
 // Annotation View
 Sidebar.AnnotationView = Backbone.View.extend({
-	tagName: 'li',
-	className: 'annotation-item',
-	initialize: function (annotation) {
-		this.commenttemplate = $('#comment-template').html();
-		this.highlighttemplate = $('#highlight-template').html();
-		this.mdconverter = new Showdown.converter();
-		this.href="#full"+this.model.get("uuid");
-	},
-	render: function () {
-		$(this.el).find("highlight.comment img").addClass("thumbnail");
+  tagName: 'li',
+  className: 'annotation-item',
+  initialize: function(annotation) {
+    this.commenttemplate = $('#comment-template').html();
+    this.highlighttemplate = $('#highlight-template').html();
+    this.mdconverter = new Showdown.converter();
+    this.href="#full"+this.model.get("uuid");
+  },
+  render: function() {
+    $(this.el).find("highlight.comment img").addClass("thumbnail");
 
-		// This annotation contains a comment
-		if (this.model.get("text") != "") {
-			this.mdConvert();
-			$(this.el).html(Mustache.to_html(this.commenttemplate, this.model.toJSON())); // instead of console.info: 
-		}
+    // This annotation contains a comment
+    if (this.model.get("text") != "") {
+      this.mdConvert();
+      $(this.el).html(Mustache.to_html(this.commenttemplate, this.model.toJSON())); // instead of console.info:
+    }
 
-		// This is just a highlight -- no contents
-		else {
-			$(this.el).html(Mustache.to_html(this.highlighttemplate, this.model.toJSON())); // instead of console.info: 
-		}
-		$(this.el).find(".details").hide();
-	 	$(this.el).find("a").click(function(){
-		  window.open(this.href, '_blank');
-		  return false;
-		});
-		return this;
-	},
-	mdConvert: function () {
-		var userComment = this.model.get("text");
-		if (userComment != "") {
-			var formattedComment = this.mdconverter.makeHtml(userComment);
-			// Temporarily converting to text-only comment due to sidebar formatting issues
-			var textComment = $(formattedComment).text()
-			this.model.set("text", textComment);
-		}
-		return this;
-	}
+    // This is just a highlight -- no contents
+    else {
+      $(this.el).html(Mustache.to_html(this.highlighttemplate, this.model.toJSON())); // instead of console.info:
+    }
+    $(this.el).find(".details").hide();
+     $(this.el).find("a").click(function(){
+      window.open(this.href, '_blank');
+      return false;
+    });
+    return this;
+  },
+  mdConvert: function() {
+    var userComment = this.model.get("text");
+    if (userComment != "") {
+      var formattedComment = this.mdconverter.makeHtml(userComment);
+      // Temporarily converting to text-only comment due to sidebar formatting issues
+      var textComment = $(formattedComment).text()
+      this.model.set("text", textComment);
+    }
+    return this;
+  }
 });
 
 // Annotation List View
 Sidebar.AnnotationListView = Backbone.View.extend({
-	el: $("div#annotation-well"),
-	initialize: function (options) {
-		this.template = $('#annotation-template').html();
-	},
-	render: function () {
-		// Clear out existing annotations
-		$("ul#annotation-list").find(".annotation-item").remove();
+  el: $("div#annotation-well"),
+  initialize: function(options) {
+    this.template = $('#annotation-template').html();
+  },
+  render: function() {
+    var collection = this.collection
+    // Clear out existing annotations
+    $("ul#annotation-list").find(".annotation-item").remove();
 
-		// Walk throught the list, and render markdown in the user comment first.
-		this.collection.each(function(ann) {
-			var annView = new Sidebar.AnnotationView({model: ann});
-			$("ul#annotation-list").append(annView.render().el);
-		});
-		// this.collection.sort();
+    // Sort the collection by where it appears in the document
+    sorted_ids = $('.annotator-hl').map(function(i,e){
+      return e.id.substring(2);
+    }).toArray();
 
-		$("li.annotation-item span").tooltip();
+    collection.each(function(ann) {
+      position = sorted_ids.indexOf(ann.get('uuid'));
+      ann.set('order', position);
+    });
 
-		// Bind some events to links
-		$("span.annotator-hl").click(function(event) {
-			$("ul#annotation-list li").removeClass('hover');
-			$("span.highlightlink").tooltip('hide');
-			var str = this.id.toString();
-			var parts = str.match(/(hl)(.+)/).slice(1);
-			var targetid = "#sb" + parts[1];
+    collection.comparator = function(model) {
+      return model.get('order');
+    };
 
-			// TODO: deal with the events in a more organized way (recompose them in functions)
-			$('div#annotation-well').animate({scrollTop:$(targetid).offset().top}, 100, function (){
-				console.info("Scroll to: "+targetid);
-				// $(targetid).parent().addClass('hover');
-				$(targetid).trigger("click");
-				// $(targetid).tooltip('show'); // disappears after 1 sec?
-			});
-		});
+    collection.sort();
 
-		// Bind some events to links
-		$("li.annotation-item").click(function(event){
-			var idtarget = $(this).find("span.highlightlink").attr("data-highlight");
+    // Walk throught the list, and render markdown in the user comment first.
+    collection.each(function(ann) {
+      var annView = new Sidebar.AnnotationView({model: ann});
+      $("ul#annotation-list").append(annView.render().el);
+    });
+    // collection.sort();
 
-			// Hide all details
-			$("ul#annotation-list li").removeClass('hover');
-			$("#annotation-well ul#annotation-list li").removeClass('focuswhite');
+    $("li.annotation-item span").tooltip();
 
-			// Hide all details
-			$("ul#annotation-list li .details").hide();
+    // Bind some events to links
+    $("span.annotator-hl").click(function(event) {
+      $("ul#annotation-list li").removeClass('hover');
+      $("span.highlightlink").tooltip('hide');
+      var str = this.id.toString();
+      var parts = str.match(/(hl)(.+)/).slice(1);
+      var targetid = "#sb" + parts[1];
 
-			// Show all comments
-			$("ul#annotation-list li .highlightlink.comment, ul#annotation-list li .highlightlink.highlight").show();
+      // TODO: deal with the events in a more organized way (recompose them in functions)
+      $('div#annotation-well').animate({scrollTop:$(targetid).offset().top}, 100, function (){
+        console.info("Scroll to: "+targetid);
+        // $(targetid).parent().addClass('hover');
+        $(targetid).trigger("click");
+        // $(targetid).tooltip('show'); // disappears after 1 sec?
+      });
+    });
 
-			// Hide these comments
-			$(this).find(".comment, .highlight").hide();
+    // Bind some events to links
+    $("li.annotation-item").click(function(event){
+      var idtarget = $(this).find("span.highlightlink").attr("data-highlight");
 
-			// Show these details
-			$(this).find(".details").show(200);
+      // Hide all details
+      $("ul#annotation-list li").removeClass('hover');
+      $("#annotation-well ul#annotation-list li").removeClass('focuswhite');
 
-			$(this).addClass("focuswhite");
+      // Hide all details
+      $("ul#annotation-list li .details").hide();
 
-			//$(this).addClass('hover');
+      // Show all comments
+      $("ul#annotation-list li .highlightlink.comment, ul#annotation-list li .highlightlink.highlight").show();
 
-			// console.info("ID target attr from list item click function: "+idtarget);
-			$("span.highlightlink").tooltip('hide');
-			
-			// $(this).removeClass('hover');
+      // Hide these comments
+      $(this).find(".comment, .highlight").hide();
 
-			// console.info("This offset top "+$(this).offset().top);
-			// console.info("IdTarget offset top "+$(idtarget).offset().top);
-			$('html,body').animate({scrollTop: $(idtarget).offset().top - 150}, 500);
-			$(".icon-comment").remove();
-			$(idtarget).prepend('<i class="icon-comment"></i>');
-			// event.stopPropagation();
-		});
-	}
+      // Show these details
+      $(this).find(".details").show(200);
+
+      $(this).addClass("focuswhite");
+
+      //$(this).addClass('hover');
+
+      // console.info("ID target attr from list item click function: "+idtarget);
+      $("span.highlightlink").tooltip('hide');
+
+      // $(this).removeClass('hover');
+
+      // console.info("This offset top "+$(this).offset().top);
+      // console.info("IdTarget offset top "+$(idtarget).offset().top);
+      $('html,body').animate({scrollTop: $(idtarget).offset().top - 150}, 500);
+      $(".icon-comment").remove();
+      $(idtarget).prepend('<i class="icon-comment"></i>');
+      // event.stopPropagation();
+    });
+  }
 });
 
 // Application
 Sidebar.App = Backbone.Router.extend({
-	// Not currently being invoked.
-	routes: {
-		'list':  'listAnnotations', 
-		'update':  'updateAnnotations'
-	},
-	// takes an array of existing annotation object literals.
-	listAnnotations: function (annotationArray) {
-		Sidebar.annotations = new Sidebar.LocalAnnotationList(annotationArray);
-		var annotationsList = new Sidebar.AnnotationListView({
-			"container": $('#annotation-well'),
-			"collection": Sidebar.annotations
-		});
-		annotationsList.render();
-		// console.info("Local: "+ Sidebar.annotations.toJSON());
-	},
-	// takes an object literal of options for an XHR request.
-	updateAnnotations: function (options) {
-		Sidebar.annotations = new Sidebar.RemoteAnnotationList(options);
-		var annotationsList = new Sidebar.AnnotationListView({
-			"container": $('#annotation-well'),
-			"collection": Sidebar.annotations
-		});
-		Sidebar.annotations.deferred.done(function () {
-			annotationsList.render();
-			// console.info("Remote: "+ Sidebar.annotations.toJSON());
-		});
-	},
+  // Not currently being invoked.
+  routes: {
+    'list':  'listAnnotations',
+    'update':  'updateAnnotations'
+  },
+  // takes an array of existing annotation object literals.
+  listAnnotations: function(annotationArray) {
+    Sidebar.annotations = new Sidebar.LocalAnnotationList(annotationArray);
+    var annotationsList = new Sidebar.AnnotationListView({
+      "container": $('#annotation-well'),
+      "collection": Sidebar.annotations
+    });
+    annotationsList.render();
+    this.showAndHideAnnotations();
+    // console.info("Local: "+ Sidebar.annotations.toJSON());
+  },
+  // takes an object literal of options for an XHR request.
+  updateAnnotations: function(options) {
+    Sidebar.annotations = new Sidebar.RemoteAnnotationList(options);
+    var annotationsList = new Sidebar.AnnotationListView({
+      "container": $('#annotation-well'),
+      "collection": Sidebar.annotations
+    });
+    Sidebar.annotations.deferred.done(function() {
+      annotationsList.render();
+      this.showAndHideAnnotations();
+      // console.info("Remote: "+ Sidebar.annotations.toJSON());
+    });
+  },
+  showAndHideAnnotations: function() {
+    if(this.filtered) {
+      $('ul#annotation-list li').each(function(index, element){
+        highlight_id = $(element).find('span:first-child').data('highlight');
+        if(isScrolledIntoView($(highlight_id))){
+          $(element).show();
+        }else{
+          $(element).hide();
+        };
+      });
+    }
+  },
+  showAllAnnotations: function() {
+    $('ul#annotation-list li').show()
+  },
 });
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ class UsersController < ApplicationController
 
   def show
     if params[:id].nil? # if there is no user id in params, show current one
-        @user = current_user
+      @user = current_user
     else
-        @user = User.find params[:id]
+      @user = User.find params[:id]
     end
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
     end
-    
-    gon.rabl "app/views/users/show.rabl", as: "user"
+
+    gon.rabl template: 'app/views/users/show.rabl', as: 'user'
   end
 
   def edit

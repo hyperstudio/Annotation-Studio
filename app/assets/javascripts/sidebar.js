feb0b5d668a798c998c4f5caf937ae04fc1ff688
Jamie Folsom <jamiefolsom@gmail.com>
2014-07-04 15:00:26 -0400

    If there are no highlights, the new viewport filter failed, and stopped execution. Fixed.

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -248,10 +248,15 @@ Sidebar.App = Backbone.Router.extend({
 });
 
 function isScrolledIntoView(elem) {
+  if (elem != undefined) {
   var docViewTop = $(window).scrollTop();
   var docViewBottom = docViewTop + $(window).height();
   var elemTop = $(elem).offset().top;
   var elemBottom = elemTop + $(elem).height();
 
   return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+  else {
+    return false
+  }
 }

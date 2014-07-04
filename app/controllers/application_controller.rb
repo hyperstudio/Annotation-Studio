0a085e85694e38738ec888654282677523dac9ae
Jamie Folsom <jamiefolsom@gmail.com>
2014-07-04 15:45:39 -0400

    Add token generation to user, document and annotation controllers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # add_breadcrumb :index, :root_path
-
-  def signed_in?
-    @now = DateTime.current().to_time.iso8601
-    @jwt = JWT.encode(
-        {
-            'consumerKey' => ENV["API_CONSUMER"],
-            'userId' => current_user.email,
-            'issuedAt' => @now,
-            'ttl' => 86400
-        },
-        ENV["API_SECRET"]
-    )
-    gon.current_user = current_user
-  end
-
   def authenticate
     redirect_to root_path, notice: "You need to be signed in" unless user_signed_in?
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :generate_token
+  # add_breadcrumb :index, :root_path
+
+  def generate_token
+    @now = DateTime.current().to_time.iso8601
+    @jwt = JWT.encode(
+        {
+            'consumerKey' => ENV["API_CONSUMER"],
+            'userId' => current_user.email,
+            'issuedAt' => @now,
+            'ttl' => 86400
+        },
+        ENV["API_SECRET"]
+    )
+    gon.current_user = current_user
+  end
+  # helper_method :signed_in?
 
   def show
     if params[:id].nil? # if there is no user id in params, show current one

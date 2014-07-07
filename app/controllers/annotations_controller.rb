5efddc60a16430d32f0e2997b9c439704084d380
Jamie Folsom <jamiefolsom@gmail.com>
2014-07-07 18:49:47 +0200

    Revert before_filters in controllers.

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,24 +1,6 @@
 class AnnotationsController < ApplicationController
   before_filter :authenticate_user!
 
-  before_filter :generate_token
-  # add_breadcrumb :index, :root_path
-
-  def generate_token
-    @now = DateTime.current().to_time.iso8601
-    @jwt = JWT.encode(
-        {
-            'consumerKey' => ENV["API_CONSUMER"],
-            'userId' => current_user.email,
-            'issuedAt' => @now,
-            'ttl' => 86400
-        },
-        ENV["API_SECRET"]
-    )
-    gon.current_user = current_user
-  end
-  # helper_method :signed_in?
-
   def index
   end
 

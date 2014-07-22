30736e56dae2ecc53003c3a07831ac8afb75faba
Jamie Folsom <jamiefolsom@gmail.com>
2014-07-22 08:41:53 -0400

    WIP left and right offcanvas menus.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery_ujs
-//= require jquery.ui.all
 //= require bootstrap
 //= require bootstrap-tagsinput
 //= require jasny-bootstrap

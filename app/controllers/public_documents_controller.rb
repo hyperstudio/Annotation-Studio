33e87d09a64de21bff25e58495cfbf07431d5334
Jamie Folsom <jamiefolsom@gmail.com>
2014-09-29 17:06:43 -0400

    WIP public docs.

--- app/controllers/public_documents_controller.rb
+++ app/controllers/public_documents_controller.rb
@@ -2,6 +2,17 @@ class PublicDocumentsController < ApplicationController
   skip_before_filter :authenticate_user!, :only => [:show, :index]
 
   def show
+    @now = DateTime.current().to_time.iso8601
+    session['guest_jwt'] = JWT.encode(
+        {
+            'consumerKey' => ENV["API_CONSUMER"],
+            'userId' => "guest@example.com",
+            'issuedAt' => @now,
+            'ttl' => 86400
+        },
+        ENV["API_SECRET"]
+    )
+
     @document = Document.public.find(params[:id])
 
     respond_to do |format|

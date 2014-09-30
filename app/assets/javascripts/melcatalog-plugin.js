cdcfd7b9d4c46e9fc97cbdd8f4606331d9b7743c
Dave Goldstein <dave@performantsoftware.com>
2014-09-30 13:47:12 -0400

    Add the MEL catalog annotation plugin and attach it as necessary

---
+++ app/assets/javascripts/melcatalog-plugin.js
@@ -0,0 +1,45 @@
+//
+// Provides the MelCatalog plugin for the annotation framework
+//
+
+Annotator.Plugin.MelCatalogue = function (element) {
+    return {
+        pluginInit: function () {
+            this.annotator
+                //.subscribe("beforeAnnotationCreated", function (annotation) {
+                //    console.info("beforeAnnotationCreated: %o", annotation)
+                //})
+                //.subscribe("annotationCreated", function (annotation) {
+                //    console.info("annotationCreated: %o", annotation)
+                //})
+                //.subscribe("beforeAnnotationUpdated", function (annotation) {
+                //    console.info("beforeAnnotationUpdated: %o", annotation)
+                //})
+                //.subscribe("annotationUpdated", function (annotation) {
+                //    console.info("annotationUpdated: %o", annotation)
+                //})
+                //.subscribe("annotationDeleted", function (annotation) {
+                //    console.info("annotationDeleted: %o", annotation)
+                //})
+                .subscribe("annotationEditorShown", function (annotation) {
+                    console.info("annotationEditorShown: %o", annotation)
+                })
+                .subscribe("annotationEditorHidden", function (annotation) {
+                    console.info("annotationEditorHidden: %o", annotation)
+                })
+                .subscribe("annotationEditorSubmit", function (annotation) {
+                    console.info("annotationEditorSubmit: %o", annotation)
+                })
+                //.subscribe("annotationViewerShown", function (annotation) {
+                //    console.info("annotationViewerShown: %o", annotation)
+                //})
+                .subscribe("annotationViewerTextField", function (annotation) {
+                    console.info("annotationViewerTextField: %o", annotation)
+                });
+        }
+    }
+};
+
+//
+// end of file
+//

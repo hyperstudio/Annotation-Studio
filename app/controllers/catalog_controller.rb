01da6e464bc505a7d1062e3d8f5d281f22e28081
Dave Goldstein <dave@performantsoftware.com>
2014-10-03 10:58:35 -0400

    Basic search dialogue and supporting behavior

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,10 +1,29 @@
+# support for MEL catalog entries
+require 'melcatalog'
+
 class CatalogController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+
+    # default search definitions
+    @search_term = ""
+    @search_types = []
+    @search_results = []
+
+    # do we need to modify the defaults...
+    @search_term = params[:term] unless params[:term].nil?
+    types = []
+    types = params[:types].split( "," ) unless params[:types].nil?
+    @search_types << :person if types.include? 'people'
+    @search_types << :artwork if types.include? 'artwork'
+
+    # do the search
+    @search_results = Melcatalog.search( params[:term], Melcatalog.configuration.default_result_limit, @search_types ) unless @search_term.empty?
+
     render "catalog/index", :layout => false
   end
 
-  def show
-  end
+  #def show
+  #end
 end

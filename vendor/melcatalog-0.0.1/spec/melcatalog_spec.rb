16509a322431b1bb260ff4a5bc19f124b61bab6a
Dave Goldstein <dave@performantsoftware.com>
2014-10-07 13:31:40 -0400

    New catalog gem which removes specification of metadata as a search param

--- vendor/melcatalog-0.0.1/spec/melcatalog_spec.rb
+++ vendor/melcatalog-0.0.1/spec/melcatalog_spec.rb
@@ -12,14 +12,14 @@ describe Melcatalog do
 
      it 'finds by wildcard' do
         term = "%"
-        results = Melcatalog.search( term, false, Melcatalog.configuration.default_result_limit )
+        results = Melcatalog.search( term, Melcatalog.configuration.default_result_limit )
         fail if results.empty?
 
      end
 
      it 'limit result count' do
        max = 1
-       results = Melcatalog.search( '%', false, max )
+       results = Melcatalog.search( "", max )
        fail if results.empty?
 
        fail if results[:text].nil? == false && results[:text].size > max
@@ -32,22 +32,36 @@ describe Melcatalog do
   describe '#explicit' do
 
     it 'get by eid' do
-      results = Melcatalog.search( '%', false, Melcatalog.configuration.default_result_limit )
-      fail if results.empty?
 
+      # get the first text entry by EID
+      metadata = Melcatalog.texts(  )
+      fail if metadata.empty?
       eid = ""
-      eid = results[:text][ 0 ]['eid'] unless results[:text].nil?
-      eid = results[:person][ 0 ]['eid'] unless results[:person].nil?
-      eid = results[:artwork][ 0 ]['eid'] unless results[:artwork].nil?
+      eid = metadata[:text][ 0 ]['eid'] unless metadata[:text].nil?
       fail if eid.empty?
-
       results = Melcatalog.get( eid )
-      fail if results.empty?
       fail if results.size != 1
+      check_texts( results[:text], true )
 
-      check_texts( results[:text] ) unless results[:text].nil?
-      check_people( results[:person] ) unless results[:person].nil?
-      check_artworks( results[:artwork] ) unless results[:artwork].nil?
+      # get the first person entry by EID
+      metadata = Melcatalog.people(  )
+      fail if metadata.empty?
+      eid = ""
+      eid = metadata[:person][ 0 ]['eid'] unless metadata[:person].nil?
+      fail if eid.empty?
+      results = Melcatalog.get( eid )
+      fail if results.size != 1
+      check_people( results[:person], true )
+
+      # get the first artwork entry by EID
+      metadata = Melcatalog.artwork(  )
+      fail if metadata.empty?
+      eid = ""
+      eid = metadata[:artwork][ 0 ]['eid'] unless metadata[:artwork].nil?
+      fail if eid.empty?
+      results = Melcatalog.get( eid )
+      fail if results.size != 1
+      check_artworks( results[:artwork], true )
 
     end
 
@@ -55,10 +69,10 @@ describe Melcatalog do
 
   describe '#metadata' do
 
-    it 'gets AAT hiararchy' do
+    it 'gets AAT tag hiararchy' do
        metadata = Melcatalog.aat_hirararchy(  )
        fail if metadata.empty?
-       process_peers( metadata )
+       process_tag_peers( metadata )
     end
 
   end
@@ -69,43 +83,45 @@ describe Melcatalog do
       metadata = Melcatalog.texts(  )
       fail if metadata.empty?
       must_exist( metadata, :text )
-      check_texts( metadata[:text] )
+      check_texts( metadata[:text], false )
     end
 
     it 'gets people metadata' do
       metadata = Melcatalog.people(  )
       fail if metadata.empty?
       must_exist( metadata, :person )
-      check_people( metadata[:person] )
+      check_people( metadata[:person], false )
     end
 
     it 'gets artwork metadata' do
       metadata = Melcatalog.artwork(  )
       fail if metadata.empty?
       must_exist( metadata, :artwork )
-      check_artworks( metadata[:artwork] )
+      check_artworks( metadata[:artwork], false )
     end
 
   end
 
   private
 
-  def check_texts( entities )
+  def check_texts( entities, contentOk )
     entities.each { | entity |
 
       must_exist( entity, 'eid' )
       must_exist( entity, 'name' )
       must_exist( entity, 'author' )
       must_exist( entity, 'witnesses' )
-      #  must_exist( entity, :publisher )
-      #  must_exist( entity, :pubdate )
-      #  must_exist( entity, :source )
-      #  must_exist( entity, :rights )
-      must_exist( entity, 'content' )
+      #  must_exist( entity, 'publisher' )
+      #  must_exist( entity, 'pubdate' )
+      #  must_exist( entity, 'source' )
+      #  must_exist( entity, 'rights' )
+
+      must_not_exist( entity, 'content' ) if contentOk == false
+      must_exist( entity, 'content' ) if contentOk == true
     }
   end
 
-  def check_people( entities )
+  def check_people( entities, contentOk )
     entities.each { | entity |
       must_exist( entity, 'eid' )
       must_exist( entity, 'name' )
@@ -113,10 +129,12 @@ describe Melcatalog do
       must_exist( entity, 'birth' )
       must_exist( entity, 'death' )
       must_exist( entity, 'nationality' )
+
+      # not sure what the content fields are
     }
   end
 
-  def check_artworks( entities )
+  def check_artworks( entities, contentOk )
     entities.each { | entity |
       must_exist( entity, 'eid' )
       must_exist( entity, 'artist' )
@@ -129,20 +147,26 @@ describe Melcatalog do
       must_exist( entity, 'permissions' )
       must_exist( entity, 'subject' )
       must_exist( entity, 'genre' )
+
+      # not sure what the content fields are
     }
   end
 
-  def process_peers( peers )
+  def process_tag_peers( peers )
     peers.each { | entity |
       must_exist( entity, :text )
-      process_peers( entity[ :nodes ] ) unless entity[ :nodes ].nil?
+      process_tag_peers( entity[ :nodes ] ) unless entity[ :nodes ].nil?
       fail if entity[ :eid ].nil? == false && entity[ :eid ].empty?
     }
   end
 
   def must_exist( obj, field )
-    fail if obj[ field ].nil?
-    fail if obj[ field ].empty?
+    fail if obj[ field ].nil? == true
+    fail if obj[ field ].empty? == true
   end
 
+  def must_not_exist( obj, field )
+    #fail if obj[ field ].nil? == false
+    #fail if obj[ field ].empty? == false
+  end
 end

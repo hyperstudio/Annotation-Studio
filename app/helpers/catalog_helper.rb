bc8a77da6e235be188cbad79ddda5c39009a8906
Dave Goldstein <dave@performantsoftware.com>
2014-10-15 10:00:38 -0400

    More formatting of image content

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,10 +1,11 @@
 module CatalogHelper
 
-  def image_display( image )
+  def image_or_text( image, text = nil )
     if has_image( image )
-      "#{image_tag image}"
+      image_tag image
     else
-      "No image available"
+      return text unless text.nil?
+      return "No image available" if text.nil?
     end
   end
 

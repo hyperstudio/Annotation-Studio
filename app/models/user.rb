ce47feedd606434c2dac997e3329c915251c25f5
Jamie Folsom <jamiefolsom@gmail.com>
2014-10-22 11:13:55 -0400

    Remove rememberable and add timeoutable to user model, so your Annotation Studio session doesn't outlast your MIT Annotation Data Store session.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable, :rememberable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :trackable, :validatable, :confirmable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :agreement, :affiliation, :password_confirmation, :remember_me, :firstname, :lastname, :rep_privacy_list, :rep_group_list, :rep_subgroup_list, :first_name_last_initial, :username

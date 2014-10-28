3d3c1b0f75ec20c235a4624292c1eff06c9c137d
Jamie Folsom <jamiefolsom@gmail.com>
2014-10-28 11:11:33 -0400

    Add mobile logic to pubdoc controller.

--- app/controllers/public_documents_controller.rb
+++ app/controllers/public_documents_controller.rb
@@ -20,4 +20,21 @@ class PublicDocumentsController < ApplicationController
       format.json { render json: @document }
     end
   end
+
+  before_filter :prepare_for_mobile
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.env["HTTP_USER_AGENT"] =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+   # request.format = :mobile if mobile_device?
+  end
+
 end

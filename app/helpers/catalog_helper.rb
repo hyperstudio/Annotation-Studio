ea7e5699096e0235df94c663d25f22f941aeae18
Dave Goldstein <dave@performantsoftware.com>
2014-11-07 15:01:47 -0500

    Fix image insert with multi-tag title

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,5 +1,14 @@
 module CatalogHelper
 
+  def tagset_display( field )
+    return "" if( field.nil? || field.empty? )
+
+    tags = field.split( "|" )
+    return field if tags.length == 1
+
+    return( "#{tags[0]}...")
+  end
+
   def image_or_text( image, text = nil )
     if has_image( image )
       image_tag( image, :class => "img-responsive" )

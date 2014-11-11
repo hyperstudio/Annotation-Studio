3a2f00bbea303f7738c1a124272de34f8bb752fd
Dave Goldstein <dave@performantsoftware.com>
2014-11-11 13:27:27 -0500

    New gem with tag support

--- vendor/melcatalog-0.0.2/lib/melcatalog.rb
+++ vendor/melcatalog-0.0.2/lib/melcatalog.rb
@@ -18,33 +18,30 @@ module Melcatalog
    end
 
    #
-   # search the MEL catalogue by term and return an array of results
+   # search the MEL catalogue and return an array of results
    #
-   # term          - the term to search for.
-   # limit         - the maximum number of results to return.
-   # entity_types  - include just these entity types. If empty then include all entity types.
+   # search_term   - the term to search for.
+   # search_tag    - the tag to search for (defaults to all tags).
    # search_fields - just search these fields for the specified term. If empty then all fields are searched.
+   # entity_types  - include just these entity types. If empty then include all entity types.
+   # limit         - the maximum number of results to return.
    #
-   def self.search_by_term( term, limit = Melcatalog.configuration.default_result_limit, entity_types = [], search_fields = [] )
+   def self.search( search_term, search_tag = "", search_fields = [], entity_types = [], limit = Melcatalog.configuration.default_result_limit )
 
-      term = "%" if term.empty?
+      term_request = search_term
+      term_request = "&search_term=#{term_request}" unless term_request.empty?
 
-      entity_request = self.request_entity_types( entity_types )
+      tag_request = search_tag
+      tag_request = "&search_tags=#{tag_request}" unless tag_request.empty?
+
+      field_request = self.build_field_list( search_fields )
+      field_request = "&search_fields=#{field_request}" unless field_request.empty?
+
+      entity_request = self.build_entity_list( entity_types )
       entity_request = "&entry_types=#{entity_request}" unless entity_request.empty?
 
-      request = "#{Melcatalog.configuration.service_endpoint}/entries?search_term=#{term}#{entity_request}&limit=#{limit}"
+      request = "#{Melcatalog.configuration.service_endpoint}/entries?limit=#{limit}#{term_request}#{field_request}#{tag_request}#{entity_request}"
       return self.transform self.rest_get request
-   end
-
-   #
-   # search the MEL catalogue by tag and return an array of results
-   #
-   # tag           - the tag to search for.
-   # limit         - the maximum number of results to return.
-   # entity_types  - include just these entity types. If empty then include all entity types.
-   # search_fields - just search these fields for the specified term. If empty then all fields are searched.
-   #
-   def self.search_by_tag( tag, limit = Melcatalog.configuration.default_result_limit, entity_types = [], search_fields = [] )
    end
 
    #
@@ -70,21 +67,30 @@ module Melcatalog
    # helper to get all the metadata for text entities
    #
    def self.texts( limit = Melcatalog.configuration.default_result_limit )
-      return self.search_by_term( "", limit, [:text] )
+     all_terms = ""
+     all_tags = ""
+     all_fields = []
+     return self.search( all_terms, all_tags, all_fields, [:text], limit )
    end
 
    #
    # helper to get all the metadata for people entities
    #
    def self.people( limit = Melcatalog.configuration.default_result_limit )
-     return self.search_by_term( "", limit, [:person] )
+     all_terms = ""
+     all_tags = ""
+     all_fields = []
+     return self.search( all_terms, all_tags, all_fields, [:person], limit )
    end
 
    #
    # helper to get all the metadata for artwork entities
    #
    def self.artwork( limit = Melcatalog.configuration.default_result_limit )
-     return self.search_by_term( "", limit, [:artwork] )
+     all_terms = ""
+     all_tags = ""
+     all_fields = []
+     return self.search( all_terms, all_tags, all_fields, [:artwork], limit )
    end
 
   private
@@ -111,7 +117,13 @@ module Melcatalog
     return result
   end
 
-  def self.request_entity_types( entity_types )
+  def self.build_field_list( search_fields )
+    result = ""
+    result = search_fields.join( "," ) unless ( search_fields.nil? || search_fields.empty? )
+    return( result )
+  end
+
+  def self.build_entity_list( entity_types )
      result = ""
      entity_types.each do | entity |
        case entity
@@ -125,7 +137,7 @@ module Melcatalog
            result = "#{result}," unless result.empty?
            result << "artworks"
          else
-           puts "Unknown entry type #{entity}"
+           puts "Unknown entity type #{entity}"
        end
      end
 

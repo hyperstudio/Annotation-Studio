29daefe67001b0644237523ef3bf41eb1dcb3936
Dave Goldstein <dave@performantsoftware.com>
2014-11-12 11:30:49 -0500

    Strip metadata from tag fields before display

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -3,8 +3,14 @@ module CatalogHelper
   def tagset_display( field, truncate = false )
     return "" if( field.nil? || field.empty? )
 
+    # partition into the individual tags
     tags = field.split( "|" )
-    return field if tags.length == 1
+
+    # strip metadata
+    tags = tags.collect { |t| t.gsub( /^\^/, "" ) }
+
+    # if we only have one tag, return that...
+    return tags[0] if tags.length == 1
 
     return( "#{tags[0]}...") if truncate == true
     return( tags.collect { |t| "'#{t}'" }.join( "," ) )

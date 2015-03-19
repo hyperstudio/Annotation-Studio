3945624bf802d23a6b7b6c54c55cbeb2ea6e9b64
Steph Skardal <steph@endpoint.com>
2015-03-19 11:07:20 -0400

    Refactor WIP: Pull code out of annotation_sidebar into JS + erb common file.

--- app/controllers/public_documents_controller.rb
+++ app/controllers/public_documents_controller.rb
@@ -23,18 +23,8 @@ class PublicDocumentsController < ApplicationController
 
   before_filter :prepare_for_mobile
 
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.env["HTTP_USER_AGENT"] =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
-   # request.format = :mobile if mobile_device?
   end
 
 end

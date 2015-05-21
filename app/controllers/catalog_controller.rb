b4390f1c0cb73765b3cb710ce02e78508b435f7d
performantDave <dave@performantsoftware.com>
2015-05-21 12:52:12 -0400

    Add support for the event entries

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -24,6 +24,7 @@ class CatalogController < ApplicationController
     types = []
     types = params[:types].split( "," ) unless params[:types].nil?
     @search_types << :artwork if types.include? 'artwork'
+    @search_types << :event if types.include? 'events'
     @search_types << :person if types.include? 'people'
     @search_types << :place if types.include? 'places'
     @search_types << :text if types.include? 'texts'
@@ -36,6 +37,7 @@ class CatalogController < ApplicationController
     # TODO
     if @onlyimages == true
       @search_results[:artwork].delete_if { |entry| has_images( entry[ 'images'] ) == false } if @search_results[:artwork].nil? == false
+      @search_results[:event].delete_if { |entry| has_images( entry[ 'images'] ) == false } if @search_results[:event].nil? == false
       @search_results[:person].delete_if { |entry| has_images( entry[ 'images'] ) == false } if @search_results[:person].nil? == false
       @search_results[:place].delete_if { |entry| has_images( entry[ 'images'] ) == false } if @search_results[:place].nil? == false
       @search_results[:text].delete_if { |entry| has_images( entry[ 'images'] ) == false } if @search_results[:text].nil? == false
@@ -49,12 +51,12 @@ class CatalogController < ApplicationController
     status, result = Melcatalog.get( params[:eid], 'stripxml' )
 
     entry = nil
-    if status == 200 && result && result[:person]
-       entry = result[:person][ 0 ]
-    elsif status == 200 && result && result[:artwork]
-       entry = result[:artwork][ 0 ]
-    elsif status == 200 && result && result[:text]
-       entry = result[:text][ 0 ]
+    if status == 200 && result
+       entry = result[:artwork][ 0 ] if result[:artwork]
+       entry = result[:event][ 0 ] if result[:event]
+       entry = result[:person][ 0 ] if result[:person]
+       entry = result[:place][ 0 ] if result[:place]
+       entry = result[:text][ 0 ] if result[:text]
     end
 
     if entry.nil?
@@ -70,27 +72,34 @@ class CatalogController < ApplicationController
     status, result = Melcatalog.get( params[:eid], 'stripxml' )
 
     entry = nil
-    if status == 200 && result && result[:artwork]
-    entry = result[:artwork][ 0 ]
-    title = "Artwork Information"
-    fieldlist = ['artist', 'artist_national_origin', 'publication', 'technique',
-                 'material', 'location_of_print', 'genre', 'subject', 'viewed', 'permissions',
-                 'owned_acquired_borrowed', 'explicit_reference', 'associated_reference', 'see_also', 'references']
-    elsif status == 200 && result && result[:person]
-      entry = result[:person][ 0 ]
-      title = "Person Information"
-      fieldlist = ['authoritative_name', 'display_name', 'forename', 'surname', 'person_type', 'description', 'birth', 'death', 'occupation',
-                   'affiliation', 'nationality', 'cultural_context', 'see_also', 'references']
-    elsif status == 200 && result && result[:place]
-      entry = result[:place][ 0 ]
-      title = "Place Information"
-      fieldlist = ['name', 'name_type', 'alternate_name', 'place_type', 'latitude', 'longitude',
-                   'note', 'see_also', 'references']
-    elsif status == 200 && result && result[:text]
-      entry = result[:text][ 0 ]
-      title = "Text Information"
-      fieldlist = ['author', 'version', 'manuscript', 'edition', 'publisher', 'place_of_publication', 'publication_date', 'permissions',
-                   'copyright', 'credit_line', 'see_also', 'content_type', 'references', 'content']
+    if status == 200 && result
+      if result[:artwork]
+         entry = result[:artwork][ 0 ]
+         title = "Artwork Information"
+         fieldlist = ['artist', 'artist_national_origin', 'publication', 'technique',
+                      'material', 'location_of_print', 'genre', 'subject', 'viewed', 'permissions',
+                      'owned_acquired_borrowed', 'explicit_reference', 'associated_reference', 'see_also', 'references']
+      elsif result[:event]
+        entry = result[:event][ 0 ]
+        title = "Event Information"
+        fieldlist = ['name', 'alternate_name', 'event_type', 'event_subtype', 'location', 'date_type', 'from_date', 'to_date', 'event_date',
+                     'see_also', 'references']
+      elsif result[:person]
+         entry = result[:person][ 0 ]
+         title = "Person Information"
+         fieldlist = ['authoritative_name', 'display_name', 'forename', 'surname', 'person_type', 'description', 'birth', 'death', 'occupation',
+                      'affiliation', 'nationality', 'cultural_context', 'see_also', 'references']
+      elsif result[:place]
+         entry = result[:place][ 0 ]
+         title = "Place Information"
+         fieldlist = ['name', 'name_type', 'alternate_name', 'place_type', 'latitude', 'longitude',
+                      'note', 'see_also', 'references']
+      elsif result[:text]
+         entry = result[:text][ 0 ]
+         title = "Text Information"
+         fieldlist = ['author', 'version', 'manuscript', 'edition', 'publisher', 'place_of_publication', 'publication_date', 'permissions',
+                      'copyright', 'credit_line', 'see_also', 'content_type', 'references', 'content']
+      end
     end
 
     if entry.nil?

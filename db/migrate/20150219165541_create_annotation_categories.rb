f15d55c3c82c183812799190877031cb1c08e1ad
Paul Rosen <paul@paulrosen.net>
2015-12-07 09:57:24 -0500

    Remove hack to get migrations to work.

--- db/migrate/20150219165541_create_annotation_categories.rb
+++ db/migrate/20150219165541_create_annotation_categories.rb
@@ -1,13 +1,10 @@
 class CreateAnnotationCategories < ActiveRecord::Migration
   def change
-    begin # TODO-PER: this is just here because something got out of sync.
     create_table :annotation_categories do |t|
       t.string :name, :null => false
       t.string :hex
       t.string :css_classes
       t.timestamps
     end
-    rescue
-      end
   end
 end

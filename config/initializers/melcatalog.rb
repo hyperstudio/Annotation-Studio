4ef92519f75811c4461a3a95e9adffc728cd5259
nicklaiacona <nick@performantsoftware.com>
2016-01-11 09:54:27 -0500

    don't initialize melcat if tenant table isn't present'

--- config/initializers/melcatalog.rb
+++ config/initializers/melcatalog.rb
@@ -1,7 +1,9 @@
-tenant = Tenant.current_tenant
+if ActiveRecord::Base.connection.table_exists? 'tenants'
+  tenant = Tenant.current_tenant
 
-if tenant && tenant.mel_catalog_enabled 
-  Melcatalog.configure do |config|
-    config.service_endpoint = tenant.mel_catalog_url
+  if tenant && tenant.mel_catalog_enabled 
+    Melcatalog.configure do |config|
+      config.service_endpoint = tenant.mel_catalog_url
+    end
   end
 end

000e4db62fa765be70fbcdb5016112a22f951ae9
Ben Silverman <bsilverm@mit.edu>
2019-06-01 20:15:56 -0400

    minor tweaks for heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.precompile = ['*.js', '*.css']
 Rails.application.config.assets.precompile += %w(active_admin.css active_admin.js users.css
 widget.js summernote.eot summernote.ttf summernote.woff glyphicons-halflings-regular.eot glyphicons-halflings-regular.woff2
 glyphicons-halflings-regular.woff glyphicons-halflings-regular.ttf documents.css

ce124e1518875e909c9efe96d376f091b9f029f9
Ben Silverman <bsilverm@mit.edu>
2019-06-17 14:52:48 -0400

    drop extension pg_stat_statements

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190531192055) do
+ActiveRecord::Schema.define(version: 20190617185106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "resource_id",   limit: 255, null: false

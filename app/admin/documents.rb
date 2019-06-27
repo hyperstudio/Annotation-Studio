da9afd420af3296e70a12606c2fa25044071337d
Ben Silverman <bsilverm@mit.edu>
2019-06-27 13:45:57 -0400

    ActiveAdmin documents cleanup; remove add/edit documents in Admin view

--- app/admin/documents.rb
+++ app/admin/documents.rb
@@ -1,14 +1,7 @@
 ActiveAdmin.register Document do
   scope :all, :default => true
+  actions :all, :except => [:edit, :new]
 
-  # scope :due_this_week do |documents|
-  #   documents.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now)
-  # end
-
-  # scope :mine do |documents|
-  #   documents.where(:user_id => current_user.id)
-  # end
-      
   filter :title
   filter :author
   filter :publication_date
@@ -16,25 +9,11 @@ ActiveAdmin.register Document do
   filter :updated_at
   filter :taggings_tag_name, label: 'Class and Group Names', :as => :check_boxes, :collection => proc { Document.rep_group_counts.map{|t| t.name.downcase}.sort! }
 
-  # batch_action :approve do |selection|
-  #   User.find(selection).each do |user|
-  #     user.set_roles = ['student']
-  #     redirect_to collection_path, :notice => "Users approved!"
-  #   end
-  # end
-  # 
-  # batch_action :make_admin do |selection|
-  #   User.find(selection).each do |user|
-  #     user.set_roles = ['admin']
-  #     redirect_to collection_path, :notice => "Admins created!"
-  #   end
-  # end
-    
   index do |t|
     selectable_column
     id_column
     column "Title", :title
-    column "Source", :source  
+    column "Source", :source
     column "Groups", :rep_group_list, :sortable => false
     column "Creation date", :created_at
     actions
@@ -51,14 +30,14 @@ ActiveAdmin.register Document do
   form do |f|
     f.inputs "Details" do
       f.input :title, :as => :string
-      f.input :rep_group_list, 
+      f.input :rep_group_list,
         :label => "Share this document with a class",
         input_html: {
         multiple: true,
         data: {
           placeholder: "Start typing to see existing classes or add new ones",
           saved: f.object.rep_group.collect{|t| { :id => t.name, :name => t.name }}.to_json,
-          url: autocomplete_tags_path 
+          url: autocomplete_tags_path
         },
         value: f.object.rep_group_list.join(', '),
         class: 'tagselect'

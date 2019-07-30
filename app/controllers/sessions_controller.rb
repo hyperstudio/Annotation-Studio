6ca5ea283280cb13ea81d0b76944ec1ec74b68b8
wellesleyhacks <33560629+pegolegoeggo@users.noreply.github.com>
2019-07-30 10:44:20 -0400

    fixed login not redirecting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < Devise::SessionsController
 
+	def new
+		super
+	end
+
 	def create
 		# #copied from devise github. can't just call super or else will throw "too many redirects error"
 		self.resource = warden.authenticate!(auth_options)
@@ -9,7 +13,6 @@ class SessionsController < Devise::SessionsController
 	 	
 	    #custom logic: pass in :invite_token if found
 		if params[:invite_token]
-			puts 'hi'
 			@location= dashboard_path(invite_token: params[:invite_token])
 		else
 			@location= after_sign_in_path_for(resource)

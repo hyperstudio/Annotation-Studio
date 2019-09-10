9b4433e59d74d65eedadec176a1cc80a9e6a3887
Ben Silverman <bsilverm@mit.edu>
2019-09-10 12:23:59 -0400

    documentList quick fix

--- app/assets/javascripts/widget.js
+++ app/assets/javascripts/widget.js
@@ -77,7 +77,10 @@ Widget.AnnotationView = Backbone.View.extend({
 		if (slug) {
 			slug = slug.split("/");
 			slug = slug[slug.length - 1];
-			var title = documentList[slug];
+			if(!documentList)
+				var title = slug;
+			else
+				var title = documentList[slug];
 			this.model.set('title', title);
 		}
 

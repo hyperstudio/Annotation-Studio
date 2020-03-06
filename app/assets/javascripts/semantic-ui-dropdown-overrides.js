b20bc65e5f3143c961d7076bbbf3b7937adaa186
Ben Silverman <bsilverm@mit.edu>
2020-03-06 01:56:06 -0500

    Deal with long group names and doc titles

---
+++ app/assets/javascripts/semantic-ui-dropdown-overrides.js
@@ -0,0 +1,3 @@
+$.fn.dropdown.settings.templates.label = function(value, text) {
+    return '<div class="label-wrapper"><div class="text">'+text+'</div>' + '<i class="delete icon"></i></div>';
+  }

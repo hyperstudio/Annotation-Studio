64b23b07d0914f7275d0d15eb01d4f83fd3345ea
Ben Silverman <bsilverm@mit.edu>
2020-04-08 15:50:48 -0400

    Update API endpoint /api/me.json

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,35 @@
 class ApiController < ApplicationController	
-  before_action :doorkeeper_authorize!
   respond_to    :json
 
   # GET /me.json
   def me
-    @user = User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-    #@user = current_user
-    #puts @user.email
+    # If logged in
+    if !current_user.nil?
+      @user = current_user
+      my_groups = []
+      @user.group_ids.each do |g|
+        my_groups << {id: g, name: Group.where(id: g).first.name}
+      end
+      respond_to do |format|
+        format.json do
+          render json: {
+            email: @user.email,
+            groups: my_groups
+          }.to_json
+        end
+      end    
+    
+    # If not logged in
+    else
+      respond_to do |format|
+        format.json do
+          render json: {
+           "error": {
+            "code": 401,
+            "message": "Login Required"
+            }}.to_json
+        end
+      end
+    end
   end
 end

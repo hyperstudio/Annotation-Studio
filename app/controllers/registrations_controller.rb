753cd2f3b60c2694c9243dbb2fd1c37a552c8ce9
Ben Silverman <bsilverm@mit.edu>
2020-07-21 15:27:10 -0400

    Allow join group by invite when signed in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,39 @@
 class RegistrationsController < Devise::RegistrationsController
+	prepend_before_action :require_no_authentication, only: [:create, :cancel]
 
 	#not sure if necessary
 	def new
- 		super
+		@invitetoken = params[:invite_token]
+    if @invitetoken 
+
+      #make sure token is valid 
+      begin
+        invite = Invite.find_by(token: @invitetoken)
+        
+        #check for expiration 
+        unless invite.expiration_date && (invite.expiration_date < Time.now)
+          org =  invite.group #find the user group attached to the invite
+          current_user.groups << org #add this user to the new user group as a member
+          flash[:alert] = 'Successfully joined group!'
+
+        else
+          flash[:error] = 'Token expired. please get new token'
+          redirect_to dashboard_path
+          return
+
+        end #end unless
+
+      rescue ActiveRecord::RecordNotUnique
+        flash[:error] = 'Already in group'
+
+      rescue NoMethodError #if token is invalid, org will call .group on a nil class which returns this error
+        flash[:error] = 'Invalid token'
+        
+      end #end begin-rescue
+			redirect_to dashboard_path
+		else
+			 super
+		end
 	end
 
 	def create 

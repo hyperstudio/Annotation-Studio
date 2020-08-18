36ab71c837d81c416dc19052171097448da82636
Ben Silverman <bsilverm@mit.edu>
2020-08-18 13:56:05 -0600

    Update Paperclip initializer config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,13 +2,15 @@ Paperclip.interpolates :tenant do |attachment, style|
   Apartment::Tenant.current
 end
 
-Paperclip::Attachment.default_options[:path] = ':class/:attachment/:tenant/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = ":class/:attachment/:tenant/:id_partition/:style/:filename"
 
-if ['production', 'staging', 'public'].include?(Rails.env)
+if ["production", "staging", "public"].include?(Rails.env)
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:s3_credentials] = {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV["S3_BUCKET_NAME"],
+    :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+    :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
   }
+  Paperclip::Attachment.default_options[:s3_region] = ENV["AWS_REGION"]
+  Paperclip::Attachment.default_options[:s3_permissions] = "public-read"
 end

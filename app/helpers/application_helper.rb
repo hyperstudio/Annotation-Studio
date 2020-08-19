a7c7330449d34d37014e673772c0d653f21a7218
Ben Silverman <bsilverm@mit.edu>
2020-08-18 18:23:43 -0600

    Fix rb formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,17 @@
 module ApplicationHelper
-
   include DeviseMailerUrlHelper
-  require 'uri'
+  require "uri"
 
-  def bootstrap_class_for flash_type
+  def bootstrap_class_for(flash_type)
     { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
-        concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
-          concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
-          concat content_tag(:span, 'Close', class: 'sr-only')
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: "alert") do
+        concat(content_tag(:button, class: "close", data: { dismiss: "alert" }) do
+          concat content_tag(:span, "&times;".html_safe, "aria-hidden" => true)
+          concat content_tag(:span, "Close", class: "sr-only")
         end)
         concat message
       end)
@@ -21,24 +20,24 @@ module ApplicationHelper
   end
 
   def render_brand
-    if ENV['BRAND_RIBBON'] && ENV['BRAND_RIBBON'] != ""
-      render partial: ENV['BRAND_RIBBON']
+    if ENV["BRAND_RIBBON"] && ENV["BRAND_RIBBON"] != ""
+      render partial: ENV["BRAND_RIBBON"]
     else
       render partial: "shared/default_brand"
     end
   end
 
   def home_banner
-    if ENV['HOME_BANNER'] && ENV['HOME_BANNER'] != ""
-      return ENV['HOME_BANNER']
+    if ENV["HOME_BANNER"] && ENV["HOME_BANNER"] != ""
+      return ENV["HOME_BANNER"]
     else
       return "home.png"
     end
   end
 
   def render_footer
-    if ENV['FOOTER'] && ENV['FOOTER'] != ""
-      render partial: ENV['FOOTER']
+    if ENV["FOOTER"] && ENV["FOOTER"] != ""
+      render partial: ENV["FOOTER"]
     else
       render partial: "shared/default_footer"
     end
@@ -50,7 +49,7 @@ module ApplicationHelper
     if notice == t("devise.sessions.signed_in") || notice == t("devise.sessions.signed_out")
       return "self_removing"
     end
-      return ""
+    return ""
   end
 
   def mobile_device?
@@ -62,37 +61,32 @@ module ApplicationHelper
     false
   end
 
-
-
-
   #group role checking
   #lots of repetition -> how to simplify???
 
   def is_owner(group_id)
     group = Group.find(group_id)
     return group.nil? ? false : group.owner_id == current_user.id
-    
   end
-
 
   def is_manager(group_id)
     if Group.find(group_id)
-        relation = Membership.find_by(group_id: group_id, user_id: current_user.id)
-        return !relation.nil? && relation.role == 'manager'
+      relation = Membership.find_by(group_id: group_id, user_id: current_user.id)
+      return !relation.nil? && relation.role == "manager"
     end
-      false #if group is not found. 
+    false #if group is not found.
   end
 
   def is_member(group_id)
     if !Group.find(group_id).nil?
-        relation = Membership.find_by(group_id: group_id, user_id: current_user.id)
-        return !relation.nil? && relation.role == 'member'
+      relation = Membership.find_by(group_id: group_id, user_id: current_user.id)
+      return !relation.nil? && relation.role == "member"
     end
-      false #if group is not found. 
+    false #if group is not found.
   end
 
   def in_group(group_id)
-      !Membership.find_by(group_id: group_id, user_id: current_user.id).nil? 
+    !Membership.find_by(group_id: group_id, user_id: current_user.id).nil?
   end
 
   #get all docs shared with current_user's groups
